16a0dc2dded13d50a2a07af6e3d60ce914c956b7
kykyryza18 <mdasha478@gmail.com>
2022-03-13 18:28:38 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(212, 383);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Информация";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(125, 401);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Создать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(231, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(557, 382);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Вариант 9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                foreach (Control k in panel1.Controls)
+                    panel1.Controls.Remove(k);
+            }
+            Random r1 = new Random();
+            for (int j = 0; j <= 2; j++)
+            {
+                for (int i = 0; i <= 3; i++)
+                {
+                    int x = r1.Next(20, 50);
+                    int y = r1.Next(20, 50);
+                    int k = r1.Next(1, 6);
+                    switch (k)
+                    {
+                        case 1:
+                            Button a = new Button();
+                            a.Parent = panel1;
+                            a.Location = new Point(40 + 200 * j, 40 + 100 * i);
+                            a.Size = new Size(x, y); break;
+                        case 2:
+                            Label b = new Label();
+                            b.Parent = panel1;
+                            b.Location = new Point(40 + 200 * j, 40 + 100 * i);
+                            b.Text = "label"; break;
+                        case 3:
+                            TextBox c = new TextBox();
+                            c.Parent = panel1;
+                            c.Location = new Point(40 + 200 * j, 40 + 100 * i);
+                            c.Size = new Size(x, y); break;
+                        case 4:
+                            CheckBox d = new CheckBox();
+                            d.Parent = panel1;
+                            d.Location = new Point(40 + 200 * j, 40 + 100 * i);
+                            d.Text = "*"; break;
+                        case 5:
+                            RadioButton f = new RadioButton();
+                            f.Parent = panel1;
+                            f.Location = new Point(40 + 200 * j, 40 + 100 * i);
+                            f.Text = "#"; break;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            int f = 0;
+            foreach (Control k in panel1.Controls)
+            {
+                if (k is Button)
+                    a++;
+                if (k is Label)
+                    b++;
+                if (k is TextBox)
+                    c++;
+                if (k is CheckBox)
+                    d++;
+                if (k is RadioButton)
+                    f++;
+            }
+            textBox1.Text = " Button: " + a.ToString() + Environment.NewLine + " Label: " + b.ToString() + Environment.NewLine + " TextBox: " + c.ToString() + Environment.NewLine + " CheckBox: " + d.ToString() + Environment.NewLine + " RadioButton: " + f.ToString() + Environment.NewLine;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
